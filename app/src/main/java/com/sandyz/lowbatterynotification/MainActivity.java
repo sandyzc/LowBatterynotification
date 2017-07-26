947a7bc49bad5886818acc8b27ff752010d054ed
santosh s c <san.461@gmail.com>
2017-07-26 18:10:03 +0530

    Initial commit

---
+++ app/src/main/java/com/sandyz/lowbatterynotification/MainActivity.java
@@ -0,0 +1,19 @@
+package com.sandyz.lowbatterynotification;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
